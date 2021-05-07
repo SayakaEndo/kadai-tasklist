6cdf367429c20b1ea29a76d13623aff55a8dc741
saya.en <saya.en1000@gmail.com>
2021-05-07 14:03:34 +0000

    text→string

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_05_07_075421) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "status"
+    t.string "status"
   end
 
 end
