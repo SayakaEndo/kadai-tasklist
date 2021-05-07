109896e7a9858225eaabfab3c93d711c1044f1c4
saya.en <saya.en1000@gmail.com>
2021-05-07 12:01:46 +0000

    add bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(status: 'test1', content: 'test1')
+Task.create(status: 'test2', content: 'test2')
+Task.create(status: 'test3', content: 'test3')
+
+(1..100).each do |number|
+  Task.create(status: 'test' + number.to_s, content: 'test' + number.to_s)
+end
