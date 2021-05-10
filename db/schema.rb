9eff99df7e0a8af4061efd4d864c2fcab501d41c
saya.en <saya.en1000@gmail.com>
2021-05-10 08:40:00 +0000

    change migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_075421) do
+ActiveRecord::Schema.define(version: 2021_05_04_074801) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
   end
 
 end
